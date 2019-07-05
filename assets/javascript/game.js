0fa9b4153b2bc44f4b32a806d3100d9d819359ea
Brian Calderon <briancalderon200@gmail.com>
2019-07-05 16:43:09 -0700

    wins, loss and counter added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,42 @@
 var random_result;
-var win;
-var lose;
+var win = 0;
+var lose = 0;
+var counter = 0;
 
 var min = 19;
 var max = 120;
 
 random_result = Math.floor(Math.random() * (max - min) + min);
-console.log(random_result);
-$("#numToguess").html('Number to guess: ', )
+//console.log(random_result);
+$("#numToguess").html('Number to guess: ' + random_result)
 
 for (i = 0; i < 4; i++) {
 
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
+    var random = Math.floor(Math.random() * 11) + 1;
+    //console.log(random);
 
     var crystal = $("<div>");
     crystal.attr("class", 'crystal')
     crystal.attr("value-random", random)
+
+    crystal.html(random);
     $(".crystals").append(crystal);
 
 }
+
+$(".crystal").on('click', function() {
+    var crystalValue = parseInt($(this).attr('value-random'));
+
+    counter += crystalValue;
+
+    console.log(counter)
+
+
+    if (counter > random_result) {
+        loss--;
+        $(".loss").html("Losses: " + loss);
+    } else if (counter === random_result) {
+        win++;
+        $(".win").html("Wins: " + win);
+    }
+})
