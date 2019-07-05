9241b32c2a76d789bd932178d70678f91b9265e2
Brian Calderon <briancalderon200@gmail.com>
2019-07-05 15:44:48 -0700

    created 4 divs dynamically using a for loop, with a random number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
-$(".diamond").on("click", function() {
-    alert("diamond")
-})
+var random_result;
+var win;
+var lose;
+
+
+for (i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 12);
+    console.log(random);
+
+    var crystal = $("<div>");
+    crystal.attr("class", 'crystal')
+    $(".crystals").append(crystal);
+
+}
