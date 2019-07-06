6fc7f19683cf31d85e2d70b87d8dd670dd88b443
Brian Calderon <briancalderon200@gmail.com>
2019-07-05 19:16:29 -0700

    reset/start function added, with guess number at random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,68 @@
 var random_result;
 var win = 0;
-var lose = 0;
+var loss = 0;
 var counter = 0;
 
 var min = 19;
 var max = 120;
 
-random_result = Math.floor(Math.random() * (max - min) + min);
-//console.log(random_result);
-$("#numToguess").html('Number to guess: ' + random_result)
 
-for (i = 0; i < 4; i++) {
 
-    var random = Math.floor(Math.random() * 11) + 1;
-    //console.log(random);
+var resetStart = function() {
 
-    var crystal = $("<div>");
-    crystal.attr("class", 'crystal')
-    crystal.attr("value-random", random)
+    $(".crystals").empty();
 
-    crystal.html(random);
-    $(".crystals").append(crystal);
+    random_result = Math.floor(Math.random() * (max - min) + min);
 
+    $("#numToguess").html('Number to guess: ' + random_result)
+
+    for (i = 0; i < 4; i++) {
+
+        var random = Math.floor(Math.random() * 11) + 1;
+        //console.log(random);
+
+
+
+        var crystal = $("<div>");
+        crystal.attr("class", 'crystal')
+        crystal.attr("value-random", random)
+
+        crystal.html(random);
+        $(".crystals").append(crystal);
+
+    }
 }
+resetStart();
 
-$(".crystal").on('click', function() {
+
+$(document).on('click', ".crystal", function() {
     var crystalValue = parseInt($(this).attr('value-random'));
 
     counter += crystalValue;
-
     console.log(counter)
 
-
     if (counter > random_result) {
+
         loss--;
+
         $(".loss").html("Losses: " + loss);
+
+        counter = 0;
+
+        alert("You Lost!")
+
+        resetStart();
+
     } else if (counter === random_result) {
+
         win++;
+
         $(".win").html("Wins: " + win);
+
+        counter = 0;
+
+        alert("You Won!");
+
+        resetStart();
     }
 })
